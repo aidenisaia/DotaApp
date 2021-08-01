9f3b5e61747c34908216252c04c2514ddae15826
Aiden Isaia <aidenisaia@gmail.com>
2021-08-01 14:26:35 -0700

    db:seed with users, posts, and comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+User.create!(
+  username: 'pudge',
+  email: 'pudge@gmail.com',
+  password_digest: 'password'
+)
+User.create!(
+  username: 'axe',
+  email: 'axe@gmail.com',
+  password_digest: 'password'
+)
+User.create!(
+  username: 'kunkka',
+  email: 'kunkka@gmail.com',
+  password_digest: 'password'
+)
+User.create!(
+  username: 'tidehunter',
+  email: 'tidehunter@gmail.com',
+  password_digest: 'password'
+)
+
+users = User.all
+user_ids = []
+users.each do |user|
+  user_ids << user.id
+end
+
+5.times do
+  Post.create!(
+  user_id: user_ids.sample,
+  title: Faker::Lorem.sentence(word_count: 3, supplemental: true),
+  body: Faker::Lorem.paragraph(sentence_count: 3, supplemental: true)
+  )
+end
+
+posts = Post.all
+post_ids = []
+posts.each do |post|
+  post_ids << post.id
+end
+
+8.times do
+  user = users.sample
+  Comment.create!(
+    user_id: user.id,
+    post_id: post_ids.sample,
+    body: Faker::Games::Dota.quote(hero: "#{user.username}")
+  )
+end
